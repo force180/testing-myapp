70ec802576ce84561ef819c219ec423057a98799
Gazi Hasin Ishrak <ext.gazi.hasin@bracu.ac.bd>
2023-06-20 09:54:38 +0600

    st1 first

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,9 @@
 
 <x-guest-layout>
 
-    <div>
-        <h4>Welcome to my app</h4>
+    <div class="flex items-center justify-center w-full h-[50vh] flex-col">
+        <h4 class="text-2xl font-medium text-orange-700">Welcome to my app</h4>
+        <p>Hey I'm in st1</p>
     </div>
-    
+
 </x-guest-layout>
